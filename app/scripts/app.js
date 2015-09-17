c6a9a3250e0de5391b94b1de8b9bb0ac1f427f0b
Didier Roche <didrocks@ubuntu.com>
2015-09-17 20:48:31 +0200

    First component and script conversion in es6 syntax

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,15 +13,15 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // Grab a reference to our auto-binding template
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  var app = document.querySelector('#app');
+  let app = document.querySelector('#app');
 
   /* TODO: send a pull request to polymer starter kit for this
     save header y position between page to restore it at the same place.
     This enables to only condense and not condense header with a smooth transition without restoring to full
     size first. */
-  var headerY = 0;
+  let headerY = 0;
 
-  app.displayInstalledToast = function() {
+  app.displayInstalledToast = () => {
     // Check to make sure caching is actually enabled—it won't be in the dev environment.
     if (!document.querySelector('platinum-sw-cache').disabled) {
       document.querySelector('#caching-complete').show();
@@ -30,12 +30,12 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
-  app.addEventListener('dom-change', function() {
+  app.addEventListener('dom-change', () => {
     console.log('Our app is ready to rock!');
   });
 
   // See https://github.com/Polymer/polymer/issues/1381
-  window.addEventListener('WebComponentsReady', function() {
+  window.addEventListener('WebComponentsReady', () => {
     // imports are loaded and elements have been registered
   });
 
@@ -43,16 +43,18 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // the appName in the middle-container and the bottom title in the bottom-container.
   // The appName is moved to top and shrunk on condensing. The bottom sub title
   // is shrunk to nothing on condensing.
-  addEventListener('paper-header-transform', function(e) {
-    var appName = document.querySelector('#mainToolbar .app-name');
-    var middleContainer = document.querySelector('#mainToolbar .middle-container');
-    var bottomTitle = document.querySelector('#mainToolbar .bottom-title');
-    var detail = e.detail;
-    var heightDiff = detail.height - detail.condensedHeight;
-    var yRatio = Math.min(1, detail.y / heightDiff);
-    var maxMiddleScale = 0.75;  // appName max size when condensed. The smaller the number the smaller the condensed size.
-    var scaleMiddle = Math.max(maxMiddleScale, (heightDiff - detail.y) / (heightDiff / (1-maxMiddleScale))  + maxMiddleScale);
-    var scaleBottom = 1 - yRatio;
+  addEventListener('paper-header-transform', e => {
+    let appName = document.querySelector('#mainToolbar .app-name');
+    let middleContainer = document.querySelector('#mainToolbar .middle-container');
+    let bottomTitle = document.querySelector('#mainToolbar .bottom-title');
+    let detail = e.detail;
+    let heightDiff = detail.height - detail.condensedHeight;
+    let yRatio = Math.min(1, detail.y / heightDiff);
+    // appName max size when condensed. The smaller the number the smaller the condensed size.
+    let maxMiddleScale = 0.75;
+    let scaleMiddle = Math.max(maxMiddleScale,
+                               (heightDiff - detail.y) / (heightDiff / (1 - maxMiddleScale))  + maxMiddleScale);
+    let scaleBottom = 1 - yRatio;
 
     // Save last header Y position
     headerY = e.detail.y;
@@ -68,8 +70,8 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   });
 
   // Scroll page to top and expand header
-  app.scrollPageToTop = function(condenseHeader) {
-    var headerPanel = document.querySelector('paper-scroll-header-panel[main]');
+  app.scrollPageToTop = condenseHeader => {
+    let headerPanel = document.querySelector('paper-scroll-header-panel[main]');
     // this allows to scroll the content to top, even if the final result is condensed
     headerPanel.scroll(headerY, false);
     if (condenseHeader) {
@@ -79,7 +81,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
     }
   };
 
-  document.addEventListener('WebComponentsReady', function() {
+  document.addEventListener('WebComponentsReady', () => {
     // initial load for home page to trigger loading animation
     document.querySelector('home-page').show();
   });
